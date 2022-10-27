287e5469866de5bc301b86a903ac8a78a4ae6173
aidata <aidata@10.121.3.204>
2022-10-27 10:41:10 +0300

    commit

--- Gitttt/src/gitt/Gitt.java
+++ Gitttt/src/gitt/Gitt.java
@@ -3,8 +3,27 @@ package gitt;
 public class Gitt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] array = {32,27,64,18,95,14,90,70,60,37};
+		
+		System.out.printf("%s%8s%n","Index","Value");
+		int j=9;
+		for(int i=0;i<array.length/2;i++) {
+			int temp=array[i];
+			array[i]=array[j];
+			array[j]=+temp;
+			j--;
+		}
+		
+		
+		for(int counter=0;counter<array.length;counter++) {
+			System.out.printf("%5d%8s%n",counter,array[counter]);
+		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
